fd8a5bc705c22e54e2ed209ceda30d50877e0532
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2017-09-26 02:55:25 -0300

    adicinando algo para ser o menu

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-        <h1>Menu aki</h1>
-    <router-view></router-view>
-    </div>
+    <v-app tollbar dark>
+        <v-toolbar fixed style="background-color: rgba(20,20,20,0.95);">
+            <v-toolbar-title class="white--text">IFlix</v-toolbar-title>
+
+            <v-menu offset-y>
+                <v-btn flat>Categorias</v-btn>
+                <v-btn flat slot="activator" >Categorias</v-btn>
+                <v-list style="background-color: rgba(20,20,20,0.9);">
+                    <v-list-tile  v-for="item in items" :key="item" @click="">
+                        <v-list-tile-title class="white--text" v-text="item"></v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar>
+        <main>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </main>
+    </v-app>
 </template>
 <script>
     export default {
-      name: 'app'
+      name: 'app',
+        data: () => ({
+            items: [
+                'Ação', 'Aventura', 'Comedia', 'Drama'
+            ]
+        })
     }
 </script>
 
