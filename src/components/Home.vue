baf118f470b0b9e3a12e45f330657b5b782e2e13
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2017-10-05 17:11:43 -0300

    modificando home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <v-flex xs12 sm12 lg12 md12>
         <v-container fluid grid-list-md>
+            <h6>Ultimos Visualizados</h6>
             <v-layout darken-2 row wrap>
                 <v-flex
                         v-bind="{ [`xs${card.flex}`]: true }"
@@ -20,7 +21,34 @@
                                 </v-layout>
 
                             </v-container>
-                            <v-progress-linear style="position: absolute;bottom: 0;" value="30" color-front="purple"
+                            <v-progress-linear style="position: absolute;bottom: 0;" value="80" color-front="purple"
+                                               color-back="blue-grey"></v-progress-linear>
+
+                        </v-card-media>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+            <h6>Lançamentos</h6>
+            <v-layout darken-2 row wrap>
+                <v-flex
+                        v-bind="{ [`xs${card.flex}`]: true }"
+                        v-for="card in cards"
+                        :key="card.title"
+                >
+                    <v-card>
+                        <v-card-media
+                                :src="card.src"
+                                height="200px"
+                        >
+                            <v-container fill-height fluid>
+                                <v-layout fill-height>
+                                    <v-flex xs12 align-end flexbox>
+                                        <span class="headline white--text" v-text="card.title"></span>
+                                    </v-flex>
+                                </v-layout>
+
+                            </v-container>
+                            <v-progress-linear style="position: absolute;bottom: 0;" value="80" color-front="purple"
                                                color-back="blue-grey"></v-progress-linear>
 
                         </v-card-media>
@@ -32,94 +60,55 @@
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data: () => ({
-      // adicionar o tanto de quadros dinamicamente atraves do tamanho da tela
-      cards: [
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
-        },
-        {
-          title: 'Esquadrão Suicida',
-          src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
-          flex: 3
+    var tamanho;
+    function reslize () {
+        var windowWidth = window.innerWidth;
+        if(windowWidth <= 520){
+            tamanho = 6;
         }
-      ]
-    })
-  }
+        else if(windowWidth <= 1000){
+            tamanho = 4;
+        }
+        else if(windowWidth > 1000){
+            tamanho = 3;
+        }
+    }
+    window.onload = reslize;
+    window.onresize = reslize;
+
+    export default {
+        name: 'app',
+        data: () => ({
+            // adicionar o tanto de quadros dinamicamente atraves do tamanho da tela
+            cards: [
+                {
+                    title: 'Esquadrão Suicida',
+                    src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
+                    flex: tamanho
+                },
+                {
+                    title: 'Esquadrão Suicida',
+                    src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
+                    flex: tamanho
+                },
+                {
+                    title: 'Esquadrão Suicida',
+                    src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
+                    flex: tamanho
+                },
+                {
+                    title: 'Esquadrão Suicida',
+                    src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
+                    flex: tamanho
+                },
+                {
+                    title: 'Esquadrão Suicida',
+                    src: 'http://sm.ign.com/t/ign_br/screenshot/a/also-heade/also-headed-our-way-at-some-point-is-an-official-suicide-squ_shv9.640.jpg',
+                    flex: tamanho
+                }
+            ]
+        })
+    }
 </script>
 
 <style>
