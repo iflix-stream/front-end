f87ef966ddf44b020260bb5f3e5803e8f8e43f0c
Marcio Lucas <marciioluucas@gmail.com>
2017-10-19 09:24:05 -0200

    Colocando caixa de adicionar favorito ao cinema.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
         <v-flex xs12 sm6 md4 lg3 xl3
                 v-for="video in filmesAndSeries"
                 :key="video.id"
-                v-on:click="dialogAssistir=true; videoSelecionado = video; updatePlayerOptionsWithSelectedVideo(video); formatarSinopse()">
+                v-on:click="renderizarCinema(video)">
           <v-card class="card-filmes">
             <v-card-media class="image-card-filme"
                           :src="video.thumbnail"
@@ -40,32 +40,47 @@
         </v-toolbar>
         <v-layout row wrap style="background-color: #212121; height: 90.4%; margin-top:64px">
 
-          <v-flex xs12 sm12 md9 lg9 style="height: 95.5%">
+          <v-flex xs12 sm12 md9 lg9 >
+            <v-container fluid grid-list-md class="grey lighten-4" style="height: 100%" >
+              <v-layout row wrap style="height: 100% !important;" ref="playerDeVideo">
+                <v-flex>
+                  <v-card>
 
-            <v-flex xs12>
-              <video-player class="vjs-custom-skin"
-                            ref="videoPlayer"
-                            :options="playerOptions"
-                            :playsinline="true"
-                            @play="onPlayerPlay($event)"
-                            @pause="onPlayerPause($event)"
-                            @ended="onPlayerEnded($event)"
-                            @loadeddata="onPlayerLoadeddata($event)"
-                            @waiting="onPlayerWaiting($event)"
-                            @playing="onPlayerPlaying($event)"
-                            @timeupdate="onPlayerTimeupdate($event)"
-                            @canplay="onPlayerCanplay($event)"
-                            @canplaythrough="onPlayerCanplaythrough($event)"
-                            @ready="playerReadied"
-                            @statechanged="playerStateChanged($event)">
-              </video-player>
-
-            </v-flex>
-            <v-flex>
-              Adicionar a minha lista, bla bla bla
-            </v-flex>
+                    <video-player class="vjs-custom-skin" fill-height fluid
+                                  ref="videoPlayer"
+                                  :options="playerOptions"
+                                  :playsinline="true"
+                                  @play="onPlayerPlay($event)"
+                                  @pause="onPlayerPause($event)"
+                                  @ended="onPlayerEnded($event)"
+                                  @loadeddata="onPlayerLoadeddata($event)"
+                                  @waiting="onPlayerWaiting($event)"
+                                  @playing="onPlayerPlaying($event)"
+                                  @timeupdate="onPlayerTimeupdate($event)"
+                                  @canplay="onPlayerCanplay($event)"
+                                  @canplaythrough="onPlayerCanplaythrough($event)"
+                                  @ready="playerReadied"
+                                  @statechanged="playerStateChanged($event)">
+                    </video-player>
+                    <v-card-actions class="purple darken-2">
+                      <v-spacer></v-spacer>
+                      <v-btn icon>
+                        <v-icon>favorite</v-icon>
+                      </v-btn>
+                      <v-btn icon>
+                        <v-icon>bookmark</v-icon>
+                      </v-btn>
+                      <v-btn icon>
+                        <v-icon>share</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
 
           </v-flex>
+
 
           <v-flex xs12 sm12 md3 lg3>
             <div style="height: 100% !important; overflow: auto">
@@ -89,17 +104,23 @@
 
                     </v-layout>
                     <v-flex xs12>
-                      <div v-if="!spoilerSinopse && !isMaiorQue183">Sinopse: {{decodeURIComponent(videoSelecionado.sinopse)}}</div>
-                      <div v-else-if="!spoilerSinopse && isMaiorQue183">Sinopse: {{decodeURIComponent(videoSelecionado.sinopsePequena)}}</div>
-                      <div v-else-if="spoilerSinopse && isMaiorQue183">Sinopse: {{decodeURIComponent(videoSelecionado.sinopseInteira)}}</div>
-                      <v-btn  v-if="!spoilerSinopse && isBotaoSpoilerSinopse" @click.native="spoilerSinopse = true" dark>
-                       Mostrar mais
+                      <div v-if="!spoilerSinopse && !isMaiorQue183">
+                        Sinopse: {{decodeURIComponent(videoSelecionado.sinopse)}}
+                      </div>
+                      <div v-else-if="!spoilerSinopse && isMaiorQue183">
+                        Sinopse: {{decodeURIComponent(videoSelecionado.sinopsePequena)}}
+                      </div>
+                      <div v-else-if="spoilerSinopse && isMaiorQue183">
+                        Sinopse: {{decodeURIComponent(videoSelecionado.sinopseInteira)}}
+                      </div>
+                      <v-btn outline v-if="!spoilerSinopse && isBotaoSpoilerSinopse"
+                             @click.native="spoilerSinopse = true" dark>
+                        Mostrar mais
                       </v-btn>
-                      <v-btn v-else-if="spoilerSinopse == true && isBotaoSpoilerSinopse" @click.native="spoilerSinopse = false" dark>
-                       Mostrar menos
+                      <v-btn outline v-else-if="spoilerSinopse == true && isBotaoSpoilerSinopse"
+                             @click.native="spoilerSinopse = false" dark>
+                        Mostrar menos
                       </v-btn>
-                      <!--<v-btn v-on:click="spoilerSinopse = true" v-if="!spoilerSinopse">Mostrar mais</v-btn>-->
-                      <!--<v-btn :click="spoilerSinopse = false" v-if="spoilerSinopse">Mostrar menos</v-btn>-->
                     </v-flex>
                   </v-container>
                 </v-card>
@@ -117,7 +138,7 @@
                     </v-list-tile-action>
                   </v-list-tile>
                   <v-list-tile v-for="episodio in temporada.episodios" v-bind:key="episodio.nome" @click="">
-                    <v-list-tile-content>
+                    <v-list-tile-content @click="setEpisode(episodio)">
                       <v-list-tile-title>{{ episodio.nome }}</v-list-tile-title>
                     </v-list-tile-content>
 
@@ -147,6 +168,7 @@
       filmesAndSeries: [],
       dialogAssistir: false,
       playerOptions: {
+//        height: '667',
         muted: false,
         language: 'en',
         playbackRates: [0.7, 1.0, 1.5, 2.0],
@@ -158,7 +180,6 @@
       }
     }),
     mounted () {
-
       this.getFilmes()
       this.getSeries()
       setTimeout(() => {
@@ -172,17 +193,29 @@
       }
     },
     methods: {
+      renderizarCinema: function (video) {
+        this.dialogAssistir = true
+        this.videoSelecionado = video
+        this.updatePlayerOptionsWithSelectedVideo(video)
+        this.formatarSinopse()
+//        this.calcularAlturaPlayer()
+      },
       updatePlayerOptionsWithSelectedVideo: function (video) {
         this.playerOptions.sources[0].src = Api.url + '/' + video.tipo + '/?stream=true&id=' + video.caminho
         this.playerOptions.poster = video.thumbnail
+
+      },
+      setEpisode: function (episodio) {
+        this.playerOptions.sources[0].src = Api.url + '/serie/?stream=true&id=' + episodio.caminho
+        console.log(episodio)
       },
       formatarSinopse: function () {
-        this.isBotaoSpoilerSinopse = false;
-        this.spoilerSinopse = false;
-        this.isMaiorQue183 = false;
+        this.isBotaoSpoilerSinopse = false
+        this.spoilerSinopse = false
+        this.isMaiorQue183 = false
         if (this.videoSelecionado.sinopse.length > 183) {
-          this.isBotaoSpoilerSinopse = true;
-          this.isMaiorQue183 = true;
+          this.isBotaoSpoilerSinopse = true
+          this.isMaiorQue183 = true
           this.videoSelecionado.sinopseInteira = this.videoSelecionado.sinopse
           this.videoSelecionado.sinopsePequena = this.videoSelecionado.sinopse.substring(0, 183) + '...'
         }
@@ -267,5 +300,18 @@
   .image-card-filme:hover {
     background-color: #000;
     opacity: 0.8;
+  }
+
+  .video-js {
+    position: relative !important;
+    width: 100% !important;
+    height: auto !important;
+  }
+  .vjs-poster {
+    position: absolute !important;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
   }
 </style>
