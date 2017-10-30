ece3551fcfa3325e834312af32198db8c53059a1
Marcio Lucas <marciioluucas@gmail.com>
2017-10-30 11:43:12 -0200

    Buildando produção.

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 export const Api = new Vue({
   data () {
     return {
-      url: 'http://iflix-api.ifapps-morrinhos.com/api',
-      shortUrl: 'http://iflix-api.ifapps-morrinhos.com/iFlix',
+      url: 'http://api-iflix.ifapps-morrinhos.com/api',
+      shortUrl: 'http://api-iflix.ifapps-morrinhos.com',
       frontUrl: 'http://192.168.1.10:8080'
     }
   }
