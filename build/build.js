a0f754a2d35de00e16cd321a7fd810f63ba73b98
Marcio Lucas <marciioluucas@gmail.com>
2017-10-30 11:11:28 -0200

    Buildando produção.

--- build/build.js
+++ build/build.js
@@ -27,11 +27,11 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log(chalk.red('  A compilação foi finalizada com erros.\n'))
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.cyan('  Compilação completa.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 export const Api = new Vue({
   data () {
     return {
-      url: 'http://192.168.1.10:8081/iFlix/api',
-      shortUrl: 'http://192.168.1.10:8081/iFlix',
+      url: 'http://iflix-api.ifapps-morrinhos.com/api',
+      shortUrl: 'http://iflix-api.ifapps-morrinhos.com/iFlix',
       frontUrl: 'http://192.168.1.10:8080'
     }
   }
